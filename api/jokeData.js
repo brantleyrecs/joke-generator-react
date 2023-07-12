f35890e15ed0a28f81a9d3e97cd795de87753194
Michael Brantley <brantleyrecs@gmail.com>
2023-07-11 20:45:44 -0500

    added more jokes

--- api/jokeData.js
+++ api/jokeData.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const endpoint = 'https://v2.jokeapi.dev/joke/Programming?safe-mode&type=twopart';
+const endpoint = 'https://v2.jokeapi.dev/joke/Programming,Miscellaneous,Pun?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=twopart';
 
 const getJoke = async () => {
   const jokeCall = await axios.get(endpoint);
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css';
+
 // import NavBar from '../components/NavBar';
 
 function MyApp({ Component, pageProps }) {
